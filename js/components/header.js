a596c6d9d83e42b1ca98b217668ff7ff6530f559
Joia <gustavo.sc.joia@gmail.com>
2025-07-02 07:06:40 -0300

    FEAT: Animação da formiga andando no header

--- js/components/header.js
+++ js/components/header.js
@@ -3,6 +3,7 @@ export const Header = {
         <header>
             <img src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
             <h1>Formigueiro Virtual</h1>
+            <img class="formiguinha treme-treme" src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
         </header>
     `
 }
