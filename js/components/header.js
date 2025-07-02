bce9c3c87f4813cbac599238cbc6715761e4d720
Joia <gustavo.sc.joia@gmail.com>
2025-07-02 07:38:28 -0300

    FIX: Correções na animação

--- js/components/header.js
+++ js/components/header.js
@@ -1,9 +1,9 @@
 export const Header = {
     template: `
-        <header>
+        <header class="overflow-hidden">
             <img src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
             <h1>Formigueiro Virtual</h1>
-            <img class="formiguinha treme-treme" src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
+            <img id="formiga" class="formiguinha treme-treme" src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
         </header>
     `
 }
