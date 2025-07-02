5f5a47d95edbeaae686646f95d6f1529a63d341f
Joia <gustavo.sc.joia@gmail.com>
2025-07-02 06:23:59 -0300

    FIX: Endereço de imagens

--- js/components/header.js
+++ js/components/header.js
@@ -1,7 +1,7 @@
 export const Header = {
     template: `
         <header>
-            <img src="/FormigueiroVirtual/img/formiga.png">
+            <img src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
             <h1>Formigueiro Virtual</h1>
         </header>
     `
