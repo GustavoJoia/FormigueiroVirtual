d74add6ce32784af7b89e83d679f05aaedb7668e
Joia <gustavo.sc.joia@gmail.com>
2025-07-02 02:53:07 -0300

    INIT: Estrutura do projeto

---
+++ js/components/header.js
@@ -0,0 +1,7 @@
+export const Header = {
+    template: `
+        <header>
+            
+        </header>
+    `
+}
---
+++ js/pages/home.js
@@ -0,0 +1,10 @@
+import { Header } from "../components/header.js"
+
+export const Home = {
+    template: `
+        <Header></Header>
+    `,
+    components:{
+        Header
+    }
+}
