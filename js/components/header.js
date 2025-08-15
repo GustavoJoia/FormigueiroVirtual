0399f504e99eefba34d6b789b3dbc6f06e749d28
Joia <gustavo.sc.joia@gmail.com>
2025-08-15 17:55:13 -0300

    FEAT: a formiga anda

--- js/components/header.js
+++ js/components/header.js
@@ -1,9 +1,9 @@
 export const Header = {
     template: `
         <header class="overflow-hidden">
-            <img src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
+            <img src="/img/formiga.png">
             <h1>Formigueiro Virtual</h1>
-            <img id="formiga" class="formiguinha treme-treme" src="https://gustavojoia.github.io/FormigueiroVirtual/img/formiga.png">
+            <img id="formiga" class="formiguinha treme-treme" src="/img/formiga.png">
         </header>
     `
 }
