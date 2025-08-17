50326508355c6133b1038a3d8e4fd3a31142fa2f
Joia <gustavo.sc.joia@gmail.com>
2025-08-16 22:56:11 -0300

    FEAT: referência de imagens

--- js/components/header.js
+++ js/components/header.js
@@ -1,12 +1,12 @@
 export const Header = {
     template: `
         <header>
-            <img src="/img/formiga.png">
+            <img src="/FormigueiroVirtual/img/formiga.png">
             <h1>Formigueiro Virtual</h1>
-            <img id="formiga" class="formiguinha treme-treme" src="/img/formiga-andando.gif">
-            <img id="formiga" class="formiguinha treme-treme delay-1" src="/img/formiga-andando.gif">
-            <img id="formiga" class="formiguinha treme-treme delay-2" src="/img/formiga-andando.gif">
-            <img id="formiga" class="formiguinha treme-treme delay-3" src="/img/formiga-andando.gif">
+            <img id="formiga" class="formiguinha treme-treme" src="/FormigueiroVirtual/img/formiga-andando.gif">
+            <img id="formiga" class="formiguinha treme-treme delay-1" src="/FormigueiroVirtual/img/formiga-andando.gif">
+            <img id="formiga" class="formiguinha treme-treme delay-2" src="/FormigueiroVirtual/img/formiga-andando.gif">
+            <img id="formiga" class="formiguinha treme-treme delay-3" src="/FormigueiroVirtual/img/formiga-andando.gif">
         </header>
     `
 }
