e92cf1b9da2686c608c2574c2fdbf3b965c3f082
Joia <gustavo.sc.joia@gmail.com>
2025-11-29 04:58:45 -0300

    fix

--- js/pages/imagens.js
+++ js/pages/imagens.js
@@ -1,10 +1,13 @@
 export const Imagens = {
     template:`
         <main class="conteudo estudos">
+        <br>
             <h4 class="informiga">Registro de mídia</h4>
             <div class="midia-scroll">
-                <img v-for="midia in midias" class="midia" :src="'/FormigueiroVirtual/img/midia/'+midia.data">
-                <video src="/FormigueiroVirtual/img/midia/16.mov" class="midia" width="128" height="128" muted></video>
+                <div v-for="(midia,index) in midias">
+                    <img v-if="index!=15" class="midia" :src="'/FormigueiroVirtual/img/midia/'+midia.data">
+                    <video v-else :src="'/FormigueiroVirtual/img/midia/'+midia.data" controls muted autoplay loop>
+                </div>
             </div>
         </main>
     `,
@@ -26,10 +29,18 @@ export const Imagens = {
                 {data:'13.jpg',selected:false},
                 {data:'14.jpg',selected:false},
                 {data:'15.jpg',selected:false},
+                {data:'16.mov',selected:false}
             ]
         }
     },
     methods:{
-
+        setActive(index){
+            this.midias.forEach(midia, i => {
+                if(midia[i].data!=midia[index].data){
+                    midia[i].selected = false;
+                }
+                midia[index].selected = true;
+            });
+        }
     },
 }
