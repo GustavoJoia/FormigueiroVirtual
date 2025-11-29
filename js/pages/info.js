1836700e675e9b2b4c84b4f01e40535a81327f73
Joia <gustavo.sc.joia@gmail.com>
2025-11-29 02:58:07 -0300

    fix

--- js/pages/info.js
+++ js/pages/info.js
@@ -1,3 +1,100 @@
 export const Info = {
-    
+    template: `
+        <main class="conteudo estudos">
+            <h4 class="informiga">Informações Técnicas</h4>
+            <br>
+            <h5 class="cartao">Nicho Ecológico</h5>
+            <div class="cartao">
+                <br><br>
+                <p>O Nicho Ecológico é o conjunto de condições e recursos que permitem a sobrevivência de uma espécie a determinado ambiente. Isso é, o papel ecológico que cada indivíduo representa no ambiente. Mesmo as espécies mais semelhantes possuem nichos ecológicos únicos e distintos, que correspondem às suas características mais específicas.</p>
+                <br>
+                <p>Se habitat é o ambiente em que os animais vivem, o nicho ecológico é a forma como eles convivem nesse ambiente e as funções que ele desempenha. Isso inclui as relações que ele mantém, como sobrevivem a predadores e sua reprodução. </p>
+                <br>
+                <p>O nicho ecológico pode ser dividido em fundamental — onde outras espécies são desconsideradas e só é considerado o conjunto de recursos que permitem a sua existência. E o nicho ecológico realizado, que considera tanto os fatores para sobrevivência quanto a competição e predação natural causadas por outras espécies. </p>
+                <br>
+                <p>Um exemplo considerando a protagonista desse site são as formigas cortadeiras. Formigas do gênero Atta, tem o nicho como cortar folhas e transportá-las para o formigueiro, onde cultivam fungos para alimentar a colônia. Assim, elas decompõe matéria orgânica e melhoram a respiração do solo com túneis, influenciando a distribuição de plantas no ambiente natural.</p>
+                <br><br>
+            </div>
+
+            <h5 class="cartao">Ecologia</h5>
+            <div class="cartao">
+            <br><br>
+            <p>Ecologia é a ciência que estuda as interações entre os seres vivos e o ambiente em que eles vivem,  dividida em autoecologia e sinecologia, podendo ser fragmentada segundo outros critérios, como taxonômico e habitat. Essa ciência reúne campos de estudo evolução, genética, citologia, anatomia e fisiologia.
+            Ecologia é dividida em níveis de organização que é dividida em população, comunidade, ecossistemas e biosfera.</p>
+            <br><br>
+            </div>
+               
+            <h5 class="cartao">Reino</h5>
+            <div class="cartao">
+            <br><br>
+            <p>A categoria superior, respeitando a ordem de: Reino > Filo (ou Divisão) > Classe > Ordem > Família > Gênero > Espécie.</p>
+            <br>
+            <p>Ela agrupa grupos com base em características semelhantes fundamentais, a nível celular, estrutural e nutricional. Nela, existem cinco reinos principais: Animalia, Plantae, Fungi, Protista e Monera.</p>
+            <br>
+            <p>As formigas pertencem ao reino Animalia, como seres pluricelulares (com muitas células), heterotróficos (que não produzem o próprio alimento), e possuem células com núcleos bem definidos. Elas dividem essa classificação com nós humanos e outros animais, como aves, peixes e répteis.</p>
+            <br><br>
+            </div> 
+
+            <h5 class="cartao">Filo</h5>
+            <div class="cartao">
+            <br><br>
+            <p>Filo é a categoria que se encaixa abaixo de reino e acima de classe na hierarquia de classificação. E agrupa organismos com um plano corporal e características evolutivas muito semelhantes entre si. Também pode ser denominado como "Divisão", especialmente no caso de plantas e fungos.</p>
+            <br>
+            <p>No caso das formigas, elas pertencem ao Filo Arthropoda, pois possuem corpos divididos em cabeça, tórax e abdômen, além das patas articuladas e exoesqueleto. Elas dividem essa divisão com aranhas, borboletas, caranguejos e centopéias.</p>
+            <br><br>
+            </div>
+
+            <h5 class="cartao">Classe</h5>
+            <div class="cartao">
+            <br><br>
+            <p>Em Classe, as características fundamentais são mais específicas no sentido estrutural e funcional. </p>
+            <br>
+            <p>As formigas pertencem a classe Insecta, que inclui também borboletas, vespas, besouros, moscas e todos os insetos, tornando-a uma das classes mais diversas do reino animal.</p>
+            <br>
+            <p>A formiga é classificada assim por possuir a divisão de seu corpo como cabeça, tórax e abdômen, por ter seis patas e asas em algum estágio da vida.</p>
+            <br><br>
+            </div>
+
+            <h5 class="cartao">Ordem</h5>
+            <div class="cartao">
+            <br><br>
+            <p>Ordem é aquela que abrange inúmeras famílias de seres vivos com características semelhantes. Essa semelhança deve ser morfológica, funcional ou genética. </p>
+            <br>
+            <p>As formigas são da ordem Hymenoptera, que também inclui abelhas e vespas. Essa ordem é definida por características como duas asas membranosas — nem sempre presentes — e um aparelho bucal funcional, com capacidade de mastigação. Além de um comportamento colonial e social complexo, organizado por funções e castas específicas.</p>
+            <br><br>
+            </div>
+
+            <h5 class="cartao">Família</h5>
+            <div class="cartao">
+            <br><br>
+            <p>Na biologia, Família determina um táxon integrado no sistema taxonômico criado por Lineau no século XVIII. Ela agrupa vários gêneros, e pode incluir subfamílias. Além disso, pode estar incluída em uma ordem.</p>
+            <br>
+            <p>A família Formicidae inclui todas as formigas. Dentro dessa família, estão os diferentes gêneros, como Camponotus (formigas carpinteiras) e Solenopsis (formigas-de-fogo), que compartilham características gerais de formigas, como o corpo dividido em cabeça, tórax e abdômen, antenas articuladas e comportamento social.</p>
+            <br><br>
+            </div>
+
+            <h5 class="cartao">Gênero</h5>
+            <div class="cartao">
+            <br><br>
+            <p>Já o gênero agrupa espécies próximas, que compartilham características semelhantes de um mesmo ancestral. Como é o caso do Homo, que inclui várias espécies de humanos — extintas e a ainda existente, Homo sapiens. </p>
+            <br>
+            <p>O gênero é como se fosse o sobrenome de uma família, enquanto a espécie é o nome completo.<br>Exemplo:</p> 
+            Gênero — <em>Atta</em>
+            <br>Espécie — <em>Atta sexdens</em>
+            <br><br>
+            </div>
+
+            <h5 class="cartao">Espécie</h5>
+            <div class="cartao">
+            <br><br>
+            <p>A espécie é a classificação mais específica entre as categorias, abrangendo indivíduos capazes de conviver e se reproduzir entre si, gerando descendentes férteis.
+            <br>Exemplo:</p>
+            <em>Acromyrmex octospinosus</em>
+            <br><em>Acromyrmex subterraneus</em>
+            <br><br>
+            <p>Essas formigas possuem o mesmo gênero (Acromyrmex), mas se diferenciam em espécie, o que define que não podem se reproduzir entre si e até mesmo possivelmente possuem hábitos distoantes.</p>
+            <br><br>
+            </div>
+        </main>
+    `,
 }
